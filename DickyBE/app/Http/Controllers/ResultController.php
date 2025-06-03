9c3f0871e8bdb93f85ede02d0d0f2f039931b6d2
ckyarr <dckygmbl.0@gmail.com>
2025-06-03 13:08:00 +0700

    add percentage to history

--- DickyBE/app/Http/Controllers/ResultController.php
+++ DickyBE/app/Http/Controllers/ResultController.php
@@ -15,7 +15,22 @@ class ResultController extends Controller
         $results = Result::with('style')
             ->where('user_id', $userId)
             ->orderBy('created_at', 'desc')
-            ->get();
+            ->get()
+            ->map(function ($result) {
+                return [
+                    'id' => $result->id,
+                    'created_at' => $result->created_at,
+                    'style' => [
+                        'gaya_belajar' => $result->style->gaya_belajar,
+                        'rekomendasi' => $result->style->rekomendasi,
+                    ],
+                    'persentase' => [
+                        'visual' => number_format(($result->total_skor_visual / 20) * 100, 0),
+                        'auditory' => number_format(($result->total_skor_auditory / 20) * 100, 0),
+                        'kinestetik' => number_format(($result->total_skor_kinestetik / 20) * 100, 0),
+                    ]
+                ];
+            });
 
         return response()->json($results);
     }
